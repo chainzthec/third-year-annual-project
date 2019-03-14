1f3900f29c5bb07fc1884c4041d43028bb2a4b04
Theo Huchard <theo.huchard@gmail.com>
2019-03-14 01:37:19 +0100

    Update docker files and interface space.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Projet Annuel
-Code source du projet annuel
+
+## Interface
+
+Pour utiliser l'interface web, se rendre dans le dossier ```interface``` et lancer en ligne de commande:
+
+```shell
+sudo docker-compose up
+```
+
+sur environnement Unix
+
+ou alors
+
+```batch
+docker-compose up
+```
+
+sur environement Windows.
+
+Et se rendre sur localhost:8000 pour obtenir le résultat visuel.
--- interface/app/settings.py
+++ interface/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ob&^-@en+ep6@tsqh5pvnuf2(_1d5u=2o++tfep%(@nyayf@=9'
+SECRET_KEY = 'c+12f5@c%btqc*nt$t4vhaq8@hgi+cf08=q1(dppqnhz2_5#l%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+	'USER': 'postgres',
+	'HOST': 'db',
+	'PORT': 5432,
     }
 }
 
