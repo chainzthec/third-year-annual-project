91ac2761fb952d5bdb974de69934ddf2c1e752b2
hakimMzabi <hakim.mzabi@etu.u-pec.fr>
2019-06-27 21:47:44 +0200

    edit2 rbf

--- Implementation/RBF/Rbf.cpp
+++ Implementation/RBF/Rbf.cpp
@@ -4,16 +4,11 @@
 #include <iostream>
 #include <vector>
 
-#include "../Librairie/Matrix.h"
+#include "./Matrix.h"
 
 using std::vector;
 using std::cout;
 using std::endl;
-
-//reg : out = la somme des poids exp(-target |Ypre-pre(n)|²)
-//classif: out = sign (reg) 
-// la correction des poids W = l'inverse(omega)*Y
-//omega : matrice des exp(-y *|Xln-Xn|)
 
 extern "C" {
 
@@ -26,7 +21,6 @@ typedef struct sampleRBF{
 	
 	double* Wo;
 	double* Wi;
-	int sizeRbfOut;
 	//double lloydCenter;
 	double gamma;
 
@@ -36,7 +30,6 @@ RBF* initRBF(double *W, int inPutSize, int outPutSize){
 	RBF* myRBF = new RBF();
 	myRBF->Wi = new double[inPutSize];
 	myRBF->Wo = new double[outPutSize];
-	myRBF->sizeRbfOut = outPutSize;
 	myRBF->gamma = 100;
 
 	for(int i = 0;i < inPutSize; i++){
@@ -49,7 +42,6 @@ RBF* initRBF(double *W, int inPutSize, int outPutSize){
 
 
 double sign(double a){
-	
 	if(a > 0){
 		return 1;
 	}
@@ -62,11 +54,12 @@ double sign(double a){
 }
 
 double distance(point* a, point* b){
-	
 	double d = 0;
-	d += sqrt(pow((a->x - b->x), 2) + pow((a->y - b->y), 2) );	
+	d = sqrt( pow( (a->x - b->x), 2) + pow( (a->y - b->y), 2) );
+	sqrt( pow( (a->x - b->x), 2) + pow( (a->y - b->y), 2) );	
 	return d;
 }
+
 
 Matrix<double> transformDoubleToMatrix(double *mat, int rows, int cols = 1) {
     int currentPos = 0;
@@ -83,79 +76,69 @@ Matrix<double> transformDoubleToMatrix(double *mat, int rows, int cols = 1) {
 
 double* RbfWeight(double *Ytrain, point* Xtrain, int sizeRbf,RBF* myRBF){
 
-	double** phi = new double* [sizeRbf];
-	double* phiToTran = new double[sizeRbf*sizeRbf];
-	for(int i = 0;i < sizeRbf; i++ ){
-
-		phi[i] = new double[sizeRbf];
+	double** teta = new double* [sizeRbf];
+	double* tetaToTran = new double[sizeRbf*sizeRbf];
+	double* distancel = new double[sizeRbf];
+	for(int i = 0;i < sizeRbf; i++ ){ 
+		teta[i] = new double[sizeRbf];
 		
 		for(int j = 0;j < sizeRbf; j++ ){
-		
-			phi[i][j] = exp(-myRBF->gamma*pow( distance(&Xtrain[i],&Xtrain[j]), 2));
-			phiToTran[i*sizeRbf+j] = phi[i][j];	
-			printf("la%f\n" ,distance(&Xtrain[i],&Xtrain[j]) );
-
+			distancel[i] = distance(&Xtrain[i], &Xtrain[j]);
+			teta[i][j] = exp(-myRBF->gamma*pow( distancel[i], 2));
+			tetaToTran[i*sizeRbf+j] = teta[i][j];	
+			
+			
 		}
-		
+		printf("=/");
 	}
 	
-	Matrix<double>phiFin = transformDoubleToMatrix(phiToTran, sizeRbf,sizeRbf);
+	Matrix<double>tetaFin = transformDoubleToMatrix(tetaToTran, sizeRbf,sizeRbf);
 	Matrix<double>YFin = transformDoubleToMatrix(Ytrain,sizeRbf,1);
-	Matrix<double>W = phiFin.getInverse() * YFin;
+	Matrix<double>W = tetaFin.getInverse() * YFin;
 
 	return W.convertToDouble();
 }
 
 void regresRBF(point* Xtrain, double* W, RBF* myRBF, int sizeRbf){
 	
-	double res;
-		for(int i = 0;i< sizeRbf; i++){
+	double *res = new double[sizeRbf];
+	double* distancel = new double[sizeRbf];
+	for(int i = 0;i < sizeRbf; i++ ){
+		distancel[i] = distance(&Xtrain[i], &Xtrain[1]);
+		
 			for(int j = 0;j< sizeRbf; j++){
-			
-				res += W[i]* exp(-myRBF->gamma*pow((distance(&Xtrain[i],&Xtrain[1])), 2));
-				printf("%f\n",res );
+				res[i] = W[i]* exp((-myRBF->gamma)*pow(distancel[i], 2));		
+				
 			}
 		}
 		
-	myRBF->Wo[0] =  res;
+	for(int i = 0 ; i< sizeRbf;i++){
+		myRBF->Wo[0] +=  res[i];	
+	}
+	
 }
 
 void classifRBF(point* Xtrain, double* W, RBF* myRBF, int sizeRbf){
 	
 	double res = 0.0;
-		for(int i = 0;i< sizeRbf; i++){
+	double* distancel = new double[sizeRbf];
+	for(int i = 0;i < sizeRbf; i++ ){
+		distancel[i] = distance(&Xtrain[i], &Xtrain[i+1]);
 			for(int j = 0;j< sizeRbf; j++){
-				res += W[i]* exp((-myRBF->gamma)*pow((distance(&Xtrain[i],&Xtrain[1])), 2));		
+				res += W[i]* exp((-myRBF->gamma)*pow((distancel[i]), 2));		
 			}
 		}
 	
 	myRBF->Wo[0] = sign(res);
 }
 
-void print(RBF *myRBF){
-	for(int i = 0; i < myRBF->sizeRbfOut; i++){
-		if(myRBF->Wo[i] == 1){
-			printf("*");		
-		
-	}else{
-		printf("/");
-	}
-}
-
-}
-double getRand(double min, double max) {
-    double val = (double) rand() / RAND_MAX;
-    val = min + val * (max - min);
-
-    return val;
-}
 
 int main (){
 //init XTrain
 //init YTrain
-	point* XTrain = new point[4];
+	point*XTrain = new point[7];
 	
-	double YTrain[4][1];
+	double YTrain[7][1];
 	
 	XTrain[0].x = 0.13984698 ;
     XTrain[0].y = 0.41485388;
@@ -169,15 +152,34 @@ int main (){
     XTrain[3].x = 0.05471647 ;
     XTrain[3].y = 0.8640708;
 
+    XTrain[4].x = 0.91900274;
+    XTrain[4].y = 0.95617945;
+
+    XTrain[5].x = 0.1753089;
+    XTrain[5].y = 0.67689523;
+	
+    XTrain[6].x = 0.25784674;
+    XTrain[6].y = 0.12366917;
+            
+  	XTrain[7].x = 0.97495302;
+    XTrain[7].y = 0.01277128;
+            
+
+
     YTrain[0][0] = 0.46119306;
- 
- 
- 
     YTrain[1][0] = 0.78636786;
     YTrain[2][0] = 0.2617359 ;
-    YTrain[3][0] = 0.25985246;
+	YTrain[3][0] = 0.25985246;
+	YTrain[4][0] = 0.28554652;
+	YTrain[5][0] = 0.57842217;
+	YTrain[6][0] = 0.35202585;
+	YTrain[7][0] = 0.11248387;
 
-    double W[4]{
+
+    double W[7]{
+            0.5,
+            0.5,
+            0.5,
             0.5,
             0.5,
             0.5,
@@ -186,42 +188,17 @@ int main (){
 
    
 
-   RBF* myRBF = initRBF(W, 4, 1);
-   double*newWeight = RbfWeight(*YTrain,XTrain,4,myRBF);
-   regresRBF(XTrain, newWeight, myRBF,4);
+   RBF* myRBF = initRBF(W, 7, 1);
+   double*newWeight = RbfWeight(*YTrain,XTrain,7,myRBF);
+   regresRBF(XTrain, newWeight, myRBF,7);
    //classifRBF(XTrain, newWeight, myRBF,4);
-   printf("reg:%f\n",myRBF->Wo[0] );  
+   printf("\nreg:%f\n",myRBF->Wo[0] );  
    //printf("classif:%f\n",myRBF->Wo[0] );  
-   print(myRBF);
 
 
 return 0;
 }
-}
-/*
-}
 
-[[0.13984698 0.41485388]
-[0.28093573 0.36177096]
-[0.25704393 0.97695092]
-[0.05471647 0.8640708 ]
-[0.91900274 0.95617945]
-[0.1753089 0.67689523]
-[0.25784674 0.12366917]
-[0.97495302 0.01277128]
-[0.08287882 0.94833339]
-[0.39418121 0.79789368]]
 
-[
- 0.46119306
- 0.78636786
- 0.2617359 
- 0.25985246
- 0.28554652
- 0.57842217
- 0.35202585
- 0.11248387
- 0.72196561
- 0.60782134
- ]
-*/
+
+}
